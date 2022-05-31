5cfe01ead0397f3abde1341480ecb78b2b8a9d5f
thewhy-thehow <primordial.motive@gmail.com>
2022-05-31 19:08:50 +0900

    master_99dan

---
+++ 99dan.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int j;
+    printf("1이상 9이하의 정수를 입력하시오: ");
+    scanf("%d", &j);
+
+    for (int i = 1; i <= 9; i++)
+    {
+        printf("%d * %d = %d\n", j, i, j * i);
+    }
+
+    return 0;
+}
